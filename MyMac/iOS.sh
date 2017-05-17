8a91ef9e957f7a2083e034ed184558ff15386445
Leon Luo <coder.leonluo@gmail.com>
2017-05-17 17:00:51 +0800

    initialize

---
+++ MyMac/iOS.sh
@@ -0,0 +1,119 @@
+#Created by leonluo
+#/bin/bash
+
+###############
+# Shell Setting  
+###############
+DATE_TIME="`date +%Y%m%d%s`"
+
+###################
+# ebaoApp Setting
+###################
+PROJECT_DIR="/Users/leonluo/mcde_app/trunk/ios"
+LOG_PATH="/Users/leonluo"
+WORKSPACE_PATH="/Users/leonluo/mcde_app/trunk/ios/McDE.xcworkspace"
+SCHEME_NAME="McDE"
+
+OPTION_PLIST_PROD="EBaoProdExportPlist.plist"
+OPTION_PLIST_UAT="EBaoUATExportPlist.plist"
+EXPORT_OPTIONS_PLIST_DIR="$PROJECT_DIR/McDE/Application/"
+
+CONFIG_EBAO_UAT="UAT"
+CONFIG_EBAO_PROD="Release"
+CONFIG_TSRI_UAT="Tsri-UAT"
+CONFIG_TSRI_PROD="Tsri-Release"
+
+UAT_IDENTITY="iPhone Developer: dewang kong (Q2R5A837S3)"
+PROD_IDENTITY="iPhone Distribution: eBaoTech Corporation (7F8W9K8TTP)"
+
+while getopts "c:p:" Option
+do 
+  case $Option in 
+    c)
+      case $OPTARG in 
+        $CONFIG_EBAO_UAT)
+          ARCHIVE_CONFIG_NAME="$CONFIG_EBAO_UAT"
+          ;;
+        $CONFIG_EBAO_PROD)
+          ARCHIVE_CONFIG_NAME="$CONFIG_EBAO_PROD"
+          ;;
+        $CONFIG_TSRI_UAT)
+          ARCHIVE_CONFIG_NAME="$CONFIG_TSRI_UAT"
+          ;;
+        $CONFIG_TSRI_PROD)
+          ARCHIVE_CONFIG_NAME="$CONFIG_TSRI_PROD"
+          ;;
+        esac
+        ;;
+    p)
+      if [ ! -x "$OPTARG" ]; then
+        mkdir $OPTARG
+      fi
+      ARCHIVE_PATH="$OPTARG/$SCHEME_NAME-$ARCHIVE_CONFIG_NAME-$DATE_TIME/$SCHEME_NAME.xcarchive"
+      IPA_EXPORT_PATH="$OPTARG/$SCHEME_NAME-$ARCHIVE_CONFIG_NAME-$DATE_TIME/"
+      ;;
+    ?)
+      echo "unknown argument $Option"
+      echo "-c <Configuration>[UAT, Release, Demo...]"
+      exit 1
+      ;;
+  esac
+done
+
+
+
+if [ $ARCHIVE_CONFIG_NAME = "$CONFIG_EBAO_UAT" ];then
+  EXPORT_OPTIONS_PLIST_PATH="$EXPORT_OPTIONS_PLIST_DIR""$OPTION_PLIST_UAT"
+elif [ $ARCHIVE_CONFIG_NAME = "$CONFIG_EBAO_PROD" ];then
+  EXPORT_OPTIONS_PLIST_PATH="$EXPORT_OPTIONS_PLIST_DIR""$OPTION_PLIST_PROD"
+elif [ $ARCHIVE_CONFIG_NAME = "$CONFIG_TSRI_UAT" ];then
+  EXPORT_OPTIONS_PLIST_PATH="$EXPORT_OPTIONS_PLIST_DIR""$OPTION_PLIST_UAT"
+elif [ $ARCHIVE_CONFIG_NAME = "$CONFIG_TSRI_PROD" ];then
+  EXPORT_OPTIONS_PLIST_PATH="$EXPORT_OPTIONS_PLIST_DIR""$OPTION_PLIST_PROD"
+else
+  EXPORT_OPTIONS_PLIST_PATH="$EXPORT_OPTIONS_PLIST_DIR""$OPTION_PLIST_UAT"
+fi
+
+echo "*******Configuration: $ARCHIVE_CONFIG_NAME  archivePath: $ARCHIVE_PATH OptionPlistPath: $EXPORT_OPTIONS_PLIST_PATH*******"
+
+# validate
+if [  -z "$ARCHIVE_CONFIG_NAME" -a "$ARCHIVE_CONFIG_NAME"=" " ];then
+  echo "********[ERROR]'configuration' must be provided********"
+  exit 1
+fi
+
+# clean before build
+if [ ! -x "$LOG_PATH" ];then
+  echo "********[WARNING]'LOG_PATH' doesnt exists, ignore 'clean before archive'********"
+  sleep 1
+else
+  xcodebuild clean -configuration "$ARCHIVE_CONFIG_NAME" -alltargets >> $LOG_PATH
+fi
+
+# build before archive
+xcodebuild -workspace "$WORKSPACE_PATH" -scheme "$SCHEME_NAME" -configuration "$ARCHIVE_CONFIG_NAME"
+
+exit 1
+
+# archive
+xcodebuild archive -workspace "$WORKSPACE_PATH" -scheme "$SCHEME_NAME" -configuration "$ARCHIVE_CONFIG_NAME" -archivePath "$ARCHIVE_PATH"
+
+#TODO - read from console - continue export ipa 
+
+###################################################################
+# export 
+# CODE_SIGN_IDENTITY=证书 
+# PROVISIONING_PROFILE=描述文件UUID
+###################################################################
+xcodebuild -exportArchive -archivePath "$ARCHIVE_PATH"  -exportPath "$IPA_EXPORT_PATH" -exportOptionsPlist "$EXPORT_OPTIONS_PLIST_PATH" 
+
+if [ "$ARCHIVE_CONFIG_NAME" = "$CONFIG_EBAO_UAT" || "$ARCHIVE_CONFIG_NAME" = "$CONFIG_TSRI_UAT" ]; then
+#upload to fir
+  echo "configuration is $ARCHIVE_CONFIG_NAME."
+fi          
+
+
+
+
+
+
---
+++ MyMac/iOS_version.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+UAT="UAT"
+RELEASE="Release"
+
+containUAT=$(echo $CONFIGURATION | grep $UAT)
+containRelease=$(echo $CONFIGURATION | grep $Release)
+
+echo "$containUAT $containRelease"
+
+if [ "$containRelease" != "" ]; then
+  echo 'Release'
+  ProdVersion=$(/usr/libexec/PlistBuddy -c "Print ProdVersion" "$INFOPLIST_FILE")
+  ProdBuildNumber=$(/usr/libexec/PlistBuddy -c "Print ProdBuildNumber" "$INFOPLIST_FILE")
+  ProdBuildNumber=$(($ProdBuildNumber + 1))
+  /usr/libexec/PlistBuddy -c "Set :ProdBuildNumber $ProdBuildNumber" "$INFOPLIST_FILE"
+  /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $ProdBuildNumber" "$INFOPLIST_FILE"
+  /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $ProdVersion.$ProdBuildNumber" "$INFOPLIST_FILE"
+elif [ "$containUAT" != "" ]; then
+  echo 'UAT'
+  UATVersion=$(/usr/libexec/PlistBuddy -c "Print UATVersion" "$INFOPLIST_FILE")
+  UATBuildNumber=$(/usr/libexec/PlistBuddy -c "Print UATBuildNumber" "$INFOPLIST_FILE")
+  UATBuildNumber=$(($UATBuildNumber + 1))
+  /usr/libexec/PlistBuddy -c "Set :UATBuildNumber $UATBuildNumber" "$INFOPLIST_FILE"
+  /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $UATBuildNumber" "$INFOPLIST_FILE"
+  /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $UATVersion.$UATBuildNumber" "$INFOPLIST_FILE"
+# else
+#   UATVersion=$(/usr/libexec/PlistBuddy -c "Print UATVersion" "$INFOPLIST_FILE")
+#   buildNumber=$(/usr/libexec/PlistBuddy -c "Print UATBuildNumber" "$INFOPLIST_FILE")
+#   buildNumber=$(($buildNumber + 1))
+#   /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$INFOPLIST_FILE"
+#   /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $UATVersion.$buildNumber" "$INFOPLIST_FILE"
+fi
+
+# platform='iOS'
+# buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$INFOPLIST_FILE")
+# currentDate=`date +%Y%m%d%H%M`
+# fileName=${currentDate}_'artf287169_dml'.'sql'
+# find ./ -name "*.sql" | xargs rm -rf
+# touch ${fileName}
+
+# deleteSql="delete from t_app_version where platform='$platform' and build_no=$buildNumber;"
+# insertSql1="insert into t_app_version(platform, build_no, version, release_date, is_force_update, is_production_version)"
+# insertSql2="values('iOS', $buildNumber, '$versionName', str_to_date('$currentDate','%Y%m%d%H%i'), '$forceUpdate', '$isProductionVersion');"
+# echo "$deleteSql" > "$fileName" | echo "$insertSql1" >> "$fileName" | echo "$insertSql2" >> "$fileName" | echo "commit;" >> "$fileName"
